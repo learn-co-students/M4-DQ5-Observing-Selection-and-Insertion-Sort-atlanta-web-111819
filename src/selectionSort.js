913582ede20b72aae30345192718dbfd787b1d46
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-03 13:08:01 -0500

    Done.

--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,18 @@
 function selectionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  for (let i = 0; i < arr.length; i++) {
+    let min = i
+    
+    for (let j = i+1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j
+      }
+    }
+
+    if (min !== i) {
+      const minval = arr[min]
+      const ival = arr[i]
+      arr[i] = minval
+      arr[min] = ival
+    }
+  }
 }
